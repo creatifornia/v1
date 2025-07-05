c3e18dc957988669a08ec8fb055f03cdcfb360bb
creatifornia <creatifornia@gmail.com>
2025-07-04 20:06:58 -0600

    sa

---
+++ init/6-reset-tabs.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",(function(){function t(){document.querySelectorAll('[name="cf-reset-tabs"]').forEach((t=>{const e=t.closest("[data-w-tab]");e&&(e.setAttribute("data-w-tab","none"),console.log("✅ Tabs reset:",e))}))}t(),document.addEventListener("cf-reset-tabs",t)}));
